dbd7f55709ae26c27f78ef55efa6a430ce828a70
Jahel Santiago Leon <47577344+jahelsantiago@users.noreply.github.com>
2022-10-17 18:26:46 -0500

    added view materia

--- src/Pages/BuscadorCursos/BuscadorCursos.js
+++ src/Pages/BuscadorCursos/BuscadorCursos.js
@@ -1,50 +1,122 @@
-import { Autocomplete, TextField } from "@mui/material";
+import { Box } from "@mui/material";
+import { minWidth } from "@mui/system";
 import React, { useState } from "react";
+import Asignatura from "./Asignatura";
+import AutocompleteInput from "./Autocomplete";
+import SelectInput from "./SelectInput";
 import useStackChips from "./StackChips";
 
-function getOptionLabelCourse(option) { return option.nombre_asignatura }
+function getOptionLabelCourse(option) {
+  return option.nombre_asignatura;
+}
+function getOptionLabelSede(option) {
+  return option.nombre_sede;
+}
+function getOptionValueSede(option) {
+  return option.id_sede;
+}
 
 export default function BuscadorCursos() {
-  const initalValue = {
+  const initalValueCourse = {
     nombre_asignatura: "",
     codigo_asignatura: "",
   };
+  const initalValueSede = {
+    nombre_sede: "",
+    id_sede: "",
+  };
 
-  const [selectedCourse, setSelectedCourse] = useState(initalValue);
+  const [selectedCourse, setSelectedCourse] = useState(initalValueCourse);
+  const [selectedSede, setSelectedSede] = useState(initalValueSede);
 
-  const recentlyAdded = useStackChips(getOptionLabelCourse)
+  const recentlyAdded = useStackChips(getOptionLabelCourse);
 
   return (
-    <div>
-      <Autocomplete
-        freeSolo
+    <Box sx = {{m:1}} display = {"flex"} flexDirection = {"column"} gap = {1}>
+      <AutocompleteInput
         value={selectedCourse}
         onChange={(e, newValue) => {
-          console.log(newValue)
-          recentlyAdded.add(newValue)
+          recentlyAdded.add(newValue);
           setSelectedCourse(newValue);
         }}
-        id="free-solo-2-demo"
-        disableClearable
         options={asignaturas}
-        getOptionLabel = {getOptionLabelCourse}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Buscador de cursos"
-            InputProps={{
-              ...params.InputProps,
-              type: "search",
-            }}
-          />
-        )}
+        getOptionLabel={getOptionLabelCourse}
+        label={"Buscador de cursos"}
       />
-      {selectedCourse.nombre_asignatura}
+      <Box display={"flex"} justifyContent = {"flex-start"} alignItems = {"center"} gap = {1}>
+        <SelectInput
+          getOptionLabel={getOptionLabelSede}
+          getOptionValue={getOptionValueSede}
+          label={"Sedes"}
+          values={sedes}
+          handleChange={(e, newValue) => {
+            setSelectedSede(newValue);
+          }}
+        />
+        <SelectInput
+          getOptionLabel={getOptionLabelSede}
+          getOptionValue={getOptionValueSede}
+          label={"Tipo de grado"}
+          values={sedes}
+          handleChange={(e, newValue) => {
+            setSelectedSede(newValue);
+          }}
+          minWidth={240}
+        />
+        <AutocompleteInput
+          value={selectedCourse}
+          onChange={(e, newValue) => {
+            recentlyAdded.add(newValue);
+            setSelectedCourse(newValue);
+          }}
+          style={{ width: 300 }}
+          options={asignaturas}
+          getOptionLabel={getOptionLabelCourse}
+          label={"Facultad"}
+        />
+        <AutocompleteInput
+          value={selectedCourse}
+          onChange={(e, newValue) => {
+            recentlyAdded.add(newValue);
+            setSelectedCourse(newValue);
+          }}
+          style={{ width: 300 }}
+          options={asignaturas}
+          getOptionLabel={getOptionLabelCourse}
+          label={"Programa"}
+        />
+      </Box>
       {recentlyAdded.component}
-      
-    </div>
+      <Asignatura asignatura={Asignatura1}/>
+    </Box>
   );
 }
+
+const Asignatura1 = {
+  codigo_asignatura: "A001",
+    nombre_asignatura: "Ingenieria de software",
+    creditos: "3",
+    descripcion: "lorem30",
+    tipo : {
+      id_tipologia: "001",
+      nombre_tipologia: "Libre eleccion"
+    }
+}
+
+const sedes = [
+  {
+    id_sede: "1",
+    nombre_sede: "Bogota",
+  },
+  {
+    id_sede: "2",
+    nombre_sede: "Palmira",
+  },
+  {
+    id_sede: "3",
+    nombre_sede: "Medellin",
+  },
+];
 
 const asignaturas = [
   {
