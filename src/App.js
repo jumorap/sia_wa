6e3033dfb24c5ec2e9654a99810c67a32cd6d467
ycuervob <ycuervob@unal.edu.co>
2022-10-17 12:24:22 -0500

    Little routes changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React from "react";
 import {
     BrowserRouter as Router,
-    Route
+    Route,
+    Switch
 } from "react-router-dom";
 import InfoPersonal from "./Pages/InfoPersonal";
 import Login from "./Pages/Session";
@@ -10,12 +11,13 @@ import Login from "./Pages/Session";
 
 function App() {
     return (
-        <>
-            <Router>
-                <Route path="/auth" component={Login}/>
-                <Route path="/info_personal" component={InfoPersonal}/>
-            </Router>
-        </>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/info_personal" component={InfoPersonal} />
+                <Route path="/*" component={() => { return <h1>404 NOT FOUND</h1> }}></Route>
+            </Switch>
+        </Router>
     );
 }
 
