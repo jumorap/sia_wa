299f9778e1d31281c3c0e49eb5adebb332442ace
jumorap <jumorap@unal.edu.co>
2022-10-24 00:30:09 -0500

    "info_personal" content edit works + post data to general gateway from info_personal check +

--- src/Middleware/index.js
+++ src/Middleware/index.js
@@ -1,4 +1,5 @@
 import { getUser, updateUser } from "./InfoPersonal/get-api";
+import { updateUserQuery } from "./InfoPersonal/queries";
 import {
   getCursosByPrograma,
   inscribirCurso,
@@ -6,12 +7,13 @@ import {
   getMateriasByPrograma,
   getMateriasLibreEleccion,
 } from "./Inscripcion/get-api";
-
 import { getHistoriaAcademica } from "./InfoAcademica/get-api";
+
 
 export {
   getUser,
   updateUser,
+  updateUserQuery,
   getCursosByPrograma,
   inscribirCurso,
   getCursoInscrito,
