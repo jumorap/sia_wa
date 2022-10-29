41b7b9a7893c65cd40f491a0da6772eae69a28a1
paulinoacuna <paulinomaven911@gmail.com>
2022-10-28 21:55:39 -0500

    new

--- src/Pages/InfoAcademica/InfoAcademica.js
+++ src/Pages/InfoAcademica/InfoAcademica.js
@@ -16,72 +16,72 @@ const getData = () => {
 
 const calificaciones01 = [
   {
-  nombre: "Parcial 1",
-  nota: "2.5",
-  porcentaje: "100"
+  _nombre: "Parcial 1",
+  _nota: "2.5",
+  _porcentaje: "100"
   }
 ]
 
 
 const calificaciones02 = [
   {
-  nombre: "Parcial 1",
-  nota: "4.2",
-  porcentaje: "30"
+  _nombre: "Parcial 1",
+  _nota: "4.2",
+  _porcentaje: "30"
   },
   {
-    nombre: "Parcial 2",
-    nota: "5.0",
-    porcentaje: "80"
+    _nombre: "Parcial 2",
+    _nota: "5.0",
+    _porcentaje: "80"
     }
 ]
 
 const asignaturas = [
   {
-    codigo: "01",
-    nombre: "Calculo Diferencial",
-    creditos: "4",
-    tipo: "Fundamentación Obligatoria",
-    periodo: "2022-2",
-    esConsolidada: "true",
-    calificaciones: calificaciones01,
-    definitiva: "4.5",
-    esAprobada: true
+    _codigo: "01",
+    _nombre: "Calculo Diferencial",
+    _creditos: "4",
+    _tipo: "Fundamentación Obligatoria",
+    _periodo: "2022-2",
+    _esConsolidada: "true",
+    _calificaciones: calificaciones01,
+    _definitiva: "4.5",
+    _esAprobada: true
   },
   {
-    codigo: "02",
-    nombre: "Ingeniería Económica",
-    creditos: "3",
-    tipo: "Fundamentación Optativa",
-    periodo: "2022-1",
-    esConsolidada: "true",
-    calificaciones: calificaciones02,
-    definitiva: "4.8",
-    esAprobada: true
+    _codigo: "02",
+    _nombre: "Ingeniería Económica",
+    _creditos: "3",
+    _tipo: "Fundamentación Optativa",
+    _periodo: "2022-1",
+    _esConsolidada: "true",
+    _calificaciones: calificaciones02,
+    _definitiva: "4.8",
+    _esAprobada: true
   },
   {
-    codigo: "03",
-    nombre: "Matematicas Discretas",
-    creditos: "3",
-    tipo: "Fundamentación Obligatoria",
-    periodo: "2021-2",
-    esConsolidada: "true",
-    calificaciones: calificaciones02,
-    definitiva: "2.7",
-    esAprobada: false
+    _codigo: "03",
+    _nombre: "Matematicas Discretas",
+    _creditos: "3",
+    _tipo: "Fundamentación Obligatoria",
+    _periodo: "2021-2",
+    _esConsolidada: "true",
+    _calificaciones: calificaciones02,
+    _definitiva: "2.7",
+    _esAprobada: false
   }
 ]
 
   const historiaAcademica = {
-    documento_identidad: "4792004165",
-    id_historia: "01",
-    id_programa: "05",
-    porcentaje_avance: "75",
-    papa: "4.1",
-    pa: "4.5",
-    semestreActual: "2022-2",
-    pappi: "3.5",
-    asignaturas: asignaturas
+    _documento_identidad: "4792004165",
+    _id_historia: "01",
+    _id_programa: "05",
+    _porcentaje_avance: "75",
+    _papa: "4.1",
+    _pa: "4.5",
+    _semestreActual: "2022-2",
+    _pappi: "3.5",
+    _asignaturas: asignaturas
   }
 
   return historiaAcademica;
@@ -94,7 +94,7 @@ const formatData = (data) => {
 
 
 
-const cards = (asignatura) => {
+const cards = (_) => {
   // let fgrades = formatData(grades);
   // let names = Object.keys(fgrades);
 
@@ -103,15 +103,15 @@ const cards = (asignatura) => {
         <Card sx={[styles.card]}>
           <CardContent sx={[styles.CardContent]}>
             <Container sx={[styles.card_left]}>
-                <Typography sx={{fontWeight: "bold"}}> {asignatura.nombre + "  (" + asignatura.codigo + ")  "}</Typography>
-                <Typography >{"Creditos: " + asignatura.creditos}</Typography>
-                <Typography >{"Tipo: " + asignatura.tipo}</Typography>
-                <Typography >{"Periodo cursado: " + asignatura.periodo}</Typography>
+                <Typography sx={{fontWeight: "bold"}}> {asignatura._nombre + "  (" + asignatura._codigo + ")  "}</Typography>
+                <Typography >{"Creditos: " + asignatura._creditos}</Typography>
+                <Typography >{"Tipo: " + asignatura._tipo}</Typography>
+                <Typography >{"Periodo cursado: " + asignatura._periodo}</Typography>
 
             </Container>
             <Container sx={[styles.card_right]}>
-                <Typography sx={{fontWeight: "bold"}}>{asignatura.definitiva}</Typography>
-                <Typography >{asignatura.esAprobada ? "Aprobada" : "Reprobada"}</Typography>
+                <Typography sx={{fontWeight: "bold"}}>{asignatura._definitiva}</Typography>
+                <Typography >{asignatura._esAprobada ? "Aprobada" : "Reprobada"}</Typography>
               
 
             </Container>
@@ -128,12 +128,20 @@ const InfoAcademica = () => {
 const [data, setData] = useState(null)
 useEffect(() => {
   // Make a single request to the API
-  if (!data) getHistoriaAcademica().then((response) => setData(response.user))
+  if (!data) getHistoriaAcademica().then((response) => setData(response))
 }, [data])
 
   console.log(data)
 
-  let historiaAcademica = data.history
+  console.log(data.history)
+
+  let historiaAcademica
+
+  if(data == null){
+    historiaAcademica = data
+  }else{
+    historiaAcademica = getData()
+  }
 
     return (
     <>
@@ -150,20 +158,20 @@ useEffect(() => {
           <Paper elevation={1}>
           <Container sx={[styles.infoBasica_right]}>
               <Container sx={[styles.infoBasica_right_card]}>
-                  <Typography sx={{fontWeight: "bold"}}>{historiaAcademica.pa}</Typography>
+                  <Typography sx={{fontWeight: "bold"}}>{historiaAcademica._pa}</Typography>
                   <Typography sx={{}}>{" PA "}</Typography>
               </Container>
             
             
               <Container sx={[styles.infoBasica_right_card]}>
-                <Typography sx={{fontWeight: "bold"}}>{historiaAcademica.papa}</Typography>
+                <Typography sx={{fontWeight: "bold"}}>{historiaAcademica._papa}</Typography>
                 <Typography sx={{}}>{"PAPA"}</Typography>
               </Container>
               
 
               
               <Container sx={[styles.infoBasica_right_card]}>
-                <Typography sx={{fontWeight: "bold"}}>{historiaAcademica.pappi}</Typography>
+                <Typography sx={{fontWeight: "bold"}}>{historiaAcademica._pappi}</Typography>
                 <Typography sx={{}}>{"PAPPI"}</Typography>
               </Container>
           </Container>
@@ -178,7 +186,7 @@ useEffect(() => {
       <Typography sx={{fontWeight: "bold", marginBottom: "20px"}}>Asignaturas</Typography>
     
     <List sx={[styles.list]}>
-    {historiaAcademica.asignaturas.map((asignatura) => {
+    {historiaAcademica._asignaturas.map((asignatura) => {
         return (
           <>
               <List component="div" justify="center" width="100%">
