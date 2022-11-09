47e85336698cfb655669ef86c47a6187166bfb9c
ycuervob <64037012+ycuervob@users.noreply.github.com>
2022-11-09 16:19:13 -0500

    Final Tweaks
    Now the with the current token we fetch another one and take the user from that, if we alter the current token we have this won't work and you will get an String that says "NOT VALID TOKEN"

--- src/Middleware/sessionAsset.js
+++ src/Middleware/sessionAsset.js
@@ -1,10 +1,19 @@
-function getUserData() {
+import { auth_refresh } from "./Session/get-api";
+
+async function getUserData() {
     let my_token = sessionStorage.getItem('TOKEN');
     if (my_token) {
-        let userData = my_token.split(".")[1];
-        userData = JSON.parse(atob(userData));
-        return userData;
-    }else{
+        let newTokenSecure = await auth_refresh({ auth_token: my_token });
+        let mynewToken = newTokenSecure?.refreshToken?.auth_token;
+        if (mynewToken) {
+            let userData = mynewToken.split(".")[1];
+            userData = JSON.parse(atob(userData));
+            console.log(userData);
+            return userData;
+        }else{
+            return "NOT VALID TOKEN"
+        }
+    } else {
         return "NOT USER";
     }
 }
