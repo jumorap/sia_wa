f0c364cbfc57f86c4710abfe513c64757d7ef465
paulinoacuna <paulinomaven911@gmail.com>
2022-11-30 19:14:33 -0500

    infoacademica fnished

--- src/Middleware/InfoAcademica/queries.js
+++ src/Middleware/InfoAcademica/queries.js
@@ -1,25 +1,41 @@
 export const queries = (args) => `
 {
-    listHistory(id:${args}, null) {
-        _documento_identidad
-        _id_historia
-        _id_programa
-        _porcentaje_avance
-        _papa
-        _pa
-        _pappi
-        _semestreActual
-        _asignaturas {
-            _codigo
-            _nombre
-            _creditos
-            _tipo
-            _periodo
-            _esConsolidada
-            _calificaciones
-            _definitiva
-            _esAprobada
-          }
-        
-    }
-}`;
+    getHistory(id: ${args.id}) {
+      _documento_identidad
+  _id_historia
+  _id_programa
+  _porcentaje_avance
+  _papa
+  _pa
+  _semestreActual
+  _pappi
+  _asignaturasInscritas {
+    _codigo
+    _nombre
+    _creditos
+    _tipo
+    _periodo
+    _esConsolidada
+    _definitiva
+    _esAprobada
+  }
+  _asignaturas{
+    _codigo
+  _nombre
+  _creditos
+  _tipo
+  _periodo
+  _esConsolidada
+  _definitiva
+  _esAprobada
+  _calificaciones{
+    _nombre
+  _porcentaje
+  _nota
+  }
+  }
+      }
+    
+  }
+  
+  `;
