b284eeae4d5b30b80a0a72468c918987c2bdba3b
jumorap <jumorap@unal.edu.co>
2022-12-05 17:08:14 -0500

    Rev. Horario

--- src/Pages/Horario/Horario.js
+++ src/Pages/Horario/Horario.js
@@ -17,9 +17,9 @@ import styles from "./styles";
 
 function createData(hora, lunes, martes, miercoles, jueves, viernes, sabado, domingo) {
     return { hora, lunes, martes, miercoles, jueves, viernes, sabado, domingo };
-  }
-  
-  const rows = [
+}
+
+const rows = [
     // createData('6:00', 61, 62, 63, 64, 65, 66, 77),
     createData('7:00', 71, 72, 73, 74, 75, 76, 77),
     //createData('8:00', 81, 82, 83, 84, 85, 86, 87),
@@ -33,16 +33,137 @@ function createData(hora, lunes, martes, miercoles, jueves, viernes, sabado, dom
     createData('16:00', 161, 162, 163, 164, 165, 166, 167),
     //createData('17:00', 171, 172, 173, 174, 175, 176, 177),
     createData('18:00', 181, 182, 183, 184, 185, 186, 187),
-   // createData('19:00', 191, 192, 193, 194, 195, 196, 197),
+    // createData('19:00', 191, 192, 193, 194, 195, 196, 197),
     createData('20:00', 201, 202, 203, 204, 205, 206, 207),
     //createData('21:00', 211, 212, 213, 214, 215, 216, 217),
     createData('22:00', 221, 222, 223, 224, 225, 226, 227),
-  ];
+];
 
+
+const getData = () => {
+
+    const Horario1 = [
+        {
+            dia: "2",
+            hora_inicio: "7",
+            hora_fin: "11",
+            salon: "401-201",
+            documento_profesor: "Astaiza Gerardo",
+            tipo: "Magistral",
+        },
+        {
+            dia: "4",
+            hora_inicio: "7",
+            hora_fin: "11",
+            salon: "401-201",
+            documento_profesor: "Astaiza Gerardo",
+            tipo: "Magistral",
+        },
+    ]
+
+    const Horario2 = [
+        {
+            dia: "1",
+            hora_inicio: "9",
+            hora_fin: "13",
+            salon: "453-303",
+            documento_profesor: "Jorge Triviño",
+            tipo: "Practica",
+        },
+        {
+            dia: "3",
+            hora_inicio: "9",
+            hora_fin: "13",
+            salon: "453-303",
+            documento_profesor: "Jorge Triviño",
+            tipo: "Practica",
+        },
+    ]
+    return [
+        { //arquisoft
+            id_curso: "Arquitecura de software",
+            codigo_asignatura: 123,
+            grupo: 2,
+            horarios: [Horario1],
+            cupos_disponibles: 0,
+            cupos_totales: 30,
+        },
+        { //calculo diferencial
+            id_curso: "Calculo Diferencial",
+            codigo_asignatura: 456,
+            grupo: 3,
+            horarios: [Horario2],
+            cupos_disponibles: 0,
+            cupos_totales: 25,
+        },
+        //   { //arquitics
+        //     id_curso: "Computación visual",
+        //     codigo_asignatura: 545,
+        //     grupo: 1,
+        //     horarios: [Horario3],
+        //     cupos_disponibles: 0,
+        //     cupos_totales: 25,
+        //   },
+    ];
+};
+
+
+const horarioHandler = (Cursos) => {
+
+    // cursos array de objetos asignatura
+
+    const vistaCursos = []
+
+    let pos_col = "-"
+    let pos_row = "-"
+
+    let pos_id = 0
+
+    let salon = "-"
+    let profesor = "-"
+    let tipo = "-"
+
+    let id_curso = "-"
+    let grupo = "-"
+
+
+    Cursos.map((curso) => {
+
+        id_curso = curso.id_curso
+        grupo = curso.grupo
+
+        curso.horarios[0].map((horario) => {
+
+            pos_col = horario.dia
+            pos_row = horario.hora_inicio
+
+            salon = horario.salon
+            profesor = horario.documento_profesor
+            tipo = horario.tipo
+
+            pos_id = pos_row + pos_col
+
+            vistaCursos.push({
+                pos_id,
+                pos_col,
+                pos_row,
+                salon,
+                profesor,
+                tipo,
+                id_curso,
+                grupo,
+            })
+        })
+
+
+    })
+
+    return vistaCursos
+}
 
 const horarioHandlerFetch = (Cursos,Asignaturas) => {
 
-  // cursos array de objetos asignatura
+    // cursos array de objetos asignatura
 
     const vistaCursos = []
 
@@ -65,17 +186,17 @@ const horarioHandlerFetch = (Cursos,Asignaturas) => {
         i++
 
         grupo = curso.grupo
-        
+
         curso.horarios.map((horario) => {
-            
+
             pos_col = horario.dia
             pos_row = horario.hora_inicio
 
             salon = horario.salon
             profesor = horario.documento_profesor
             tipo = horario.tipo
-            
-            
+
+
             pos_id = (pos_row*10) + pos_col
 
             vistaCursos.push({
@@ -84,13 +205,13 @@ const horarioHandlerFetch = (Cursos,Asignaturas) => {
                 pos_row,
                 salon,
                 profesor,
-                tipo,   
+                tipo,
                 id_curso,
-                grupo, 
+                grupo,
             })
         })
-        
-        
+
+
     })
 
     return vistaCursos
@@ -98,33 +219,41 @@ const horarioHandlerFetch = (Cursos,Asignaturas) => {
 
 const createCard = (vistaCursos, row, dia) => {
 
-let id_curso  = null
-let grupo = null
-let salon = null
-let profesor = null
-let tipo = null
+    let id_curso  = null
+    let grupo = null
+    let salon = null
+    let profesor = null
+    let tipo = null
 
-let finded = false
+    let finded = false
     //dia col | hora inicio fila
-    
-    vistaCursos.map((vistaCurso) => {
-         if ((vistaCurso.pos_id == row.lunes ||
-             vistaCurso.pos_id == row.martes ||
-             vistaCurso.pos_id == row.miercoles ||
-             vistaCurso.pos_id == row.jueves ||
-             vistaCurso.pos_id == row.viernes||
-             vistaCurso.pos_id == row.sabado ||
-             vistaCurso.pos_id == row.domingo) && vistaCurso.pos_col == dia) {
-                finded = true
-                id_curso = vistaCurso.id_curso
-                grupo = vistaCurso.grupo
-                salon = vistaCurso.salon
-                profesor = vistaCurso.profesor
-                tipo = vistaCurso.tipo
-             }
-     })
 
-     return (
+    vistaCursos.map((vistaCurso) => {
+
+
+
+        if ((vistaCurso.pos_id == row.lunes ||
+            vistaCurso.pos_id == row.martes ||
+            vistaCurso.pos_id == row.miercoles ||
+            vistaCurso.pos_id == row.jueves ||
+            vistaCurso.pos_id == row.viernes||
+            vistaCurso.pos_id == row.sabado ||
+            vistaCurso.pos_id == row.domingo) && vistaCurso.pos_col == dia) {
+            finded = true
+
+
+
+            id_curso = vistaCurso.id_curso
+            grupo = vistaCurso.grupo
+            salon = vistaCurso.salon
+            profesor = vistaCurso.profesor
+            tipo = vistaCurso.tipo
+
+
+        }
+    })
+
+    return (
         <Container sx={[styles.container]}>
             <Typography fontWeight="bold" variant="body2" >{finded && id_curso}</Typography>
             <Typography variant="body2" >{finded && "Grupo: "+ grupo}</Typography>
@@ -132,100 +261,132 @@ let finded = false
             <Typography variant="body2" >{finded && "Profesor: " + profesor}</Typography>
             <Typography variant="body2" >{finded && "Clase " + tipo}</Typography>
         </Container>
-     )
+    )
 }
 
 const Horario = () => {
 
 
 //fetch historia academica
-const [data, setData] = useState(null)
-useEffect(() => {
+    const [data, setData] = useState(null)
+    useEffect(() => {
 
-  if(!data){
-  getHistoriaAcademica(sessionStorage.USER).then((response) => {
-    
-    setData(response.getHistory[0])
-  })
-}
+        if(data == null || data == undefined){
+            getHistoriaAcademica(sessionStorage.USER).then((response) => {
 
-}, [data])
+                setData(response.getHistory[0])
+            })
+        }
+
+    }, [data])
 
 
 
 //fetch cursos
-const [cursos, setCursos] = useState([])
-const [asignaturas, setAsignaturas] = useState([])
-const [vistaCursos,setVistaCursos] = useState([])
+    const [cursos, setCursos] = useState([])
+    const [asignaturas, setAsignaturas] = useState([])
+    const [vistaCursos,setVistaCursos] = useState([])
 
-useEffect(() => {
-  
-    if(data){
-     
-      data?._asignaturasInscritas.forEach(asignatura => {
+    useEffect(() => {
 
-      getCursosByCodigoAsignatura(asignatura._id_asignature).then((response) => {
-      
-      let cursosdeAsignatura_i = response.cursosByCodigoAsignatura
-  
-      cursosdeAsignatura_i.map(cursito => {
-          //buscar id curso
-          
-          if(cursito.id_curso === asignatura._codigo){
-              cursos.push(cursito)
-              setCursos(cursos)
-              asignaturas.push(asignatura)
-              setAsignaturas(asignaturas)
-          }
-      });
+        if(data != null || data != undefined){
 
-      setVistaCursos(horarioHandlerFetch(cursos,asignaturas))
-      console.log("vistacursos: ")
-      console.log(vistaCursos)
+            data?._asignaturasInscritas.forEach(asignatura => {
 
-      })
-      }
-    )}
-  }, [cursos,data,asignaturas])
 
-      return (
-      <>
-    <TableContainer component={Paper}>
-      <Table sx={{ }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">Hora</TableCell>
-            <TableCell align="center">Lunes</TableCell>
-            <TableCell align="center">Martes</TableCell>
-            <TableCell align="center">Miercoles</TableCell>
-            <TableCell align="center">Jueves</TableCell>
-            <TableCell align="center">Viernes</TableCell>
-            <TableCell align="center">Sabado</TableCell>
-            <TableCell align="center">Domingo</TableCell>
-          </TableRow>
-        </TableHead>
+                    //obtener _id_asignature: Int por cada asignatura y extraer cursos == a asignatura._codigo
 
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.hora}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row">{row.hora}</TableCell>
-              <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "1")}</TableCell>
-              <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "2")}</TableCell>
-              <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "3")}</TableCell>
-              <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "4")}</TableCell>
-              <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "5")}</TableCell>
-              <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "6")}</TableCell>
-              <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "7")}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                    // getCursosByCodigoAsignatura  asignatura._codigo
 
-      </>
-      )
-  }
-  
-  export default Horario
+
+                    // console.log(asignatura._id_asignature)
+                    // console.log(asignatura._codigo)
+
+                    getCursosByCodigoAsignatura(asignatura._id_asignature).then((response) => {
+
+                        let cursosdeAsignatura_i = response.cursosByCodigoAsignatura
+
+                        cursosdeAsignatura_i.map(cursito => {
+                            //buscar id curso
+
+                            if(cursito.id_curso == asignatura._codigo){
+
+                                cursos.push(cursito)
+                                setCursos(cursos)
+                                asignaturas.push(asignatura)
+                                setAsignaturas(asignaturas)
+
+
+                            }
+                        });
+
+
+                        setVistaCursos(horarioHandlerFetch(cursos,asignaturas))
+                        console.log("vistacursos: ")
+                        console.log(vistaCursos)
+
+
+                    })
+
+                }
+
+
+
+
+            );
+
+
+
+
+        }
+    }, [cursos,data,asignaturas])
+
+
+
+
+    console.log("cursos: ")
+    console.log(cursos)
+
+
+
+    return (
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Hora</TableCell>
+                            <TableCell align="center">Lunes</TableCell>
+                            <TableCell align="center">Martes</TableCell>
+                            <TableCell align="center">Miercoles</TableCell>
+                            <TableCell align="center">Jueves</TableCell>
+                            <TableCell align="center">Viernes</TableCell>
+                            <TableCell align="center">Sabado</TableCell>
+                            <TableCell align="center">Domingo</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.hora}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                <TableCell component="th" scope="row">{row.hora}</TableCell>
+                                <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "1")}</TableCell>
+                                <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "2")}</TableCell>
+                                <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "3")}</TableCell>
+                                <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "4")}</TableCell>
+                                <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "5")}</TableCell>
+                                <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "6")}</TableCell>
+                                <TableCell sx={[styles.dia]} >{createCard(vistaCursos, row, "7")}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+        </>
+    )
+}
+
+export default Horario
